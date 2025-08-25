c9e33417c22db798938714df04805784f0635f81
Oswi777 <contreras.o@uadec.edu.mx>
2025-08-25 14:24:40 -0600

    api reportes

--- reportes/js/config.js
+++ reportes/js/config.js
@@ -1,6 +1,7 @@
 // Config global (ajusta apiBase en tu entorno)
 window.APP_CONFIG = {
-  apiBase: "http://localhost:8000",
+//apiBase: "http://localhost:8000",
+  apiBase: "https://encuestas-reportes.onrender.com",
   autoRefreshMs: 10000,
   perPage: 10
 };
